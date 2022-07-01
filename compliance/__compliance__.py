5151574d6975861966b6f47bed892176145e967e
Pradeep Reddy Raamana <raamana@gmail.com>
2022-07-01 15:58:45 -0400

    making entry point work

---
+++ compliance/__compliance__.py
@@ -0,0 +1,17 @@
+from sys import version_info
+
+if version_info.major > 2:
+    from compliance import cli
+else:
+    raise NotImplementedError('Protocol Compliance requires Python 3 or higher. '
+                              'Upgrade to Python 3+ or use [virtual] environments.')
+
+
+def main():
+    """Entry point."""
+
+    cli.main()
+
+
+if __name__ == '__main__':
+    main()
