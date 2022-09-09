460601807cd30741345f3b221e9a18bd4bf4da36
Harsh Sinha <harsh.sinha@pitt.edu>
2022-09-09 13:01:43 -0400

    Rename imagingqa to mrQA in latex info

--- docs/conf.py
+++ docs/conf.py
@@ -135,8 +135,8 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'imagingqa.tex',
-     'ImagingQA Documentation',
+    (master_doc, 'mrQA.tex',
+     'mrQA Documentation',
      'Harsh Sinha', 'manual'),
 ]
 
@@ -145,8 +145,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'imagingqa',
-     'ImagingQA Documentation',
+    (master_doc, 'mrQA',
+     'mrQA Documentation',
      [author], 1)
 ]
 
@@ -156,10 +156,10 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'imagingqa',
-     'ImagingQA Documentation',
+    (master_doc, 'mrQA',
+     'mrQA Documentation',
      author,
-     'imagingqa',
+     'mrQA',
      'One line description of project.',
      'Miscellaneous'),
 ]
