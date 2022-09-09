543033237f2bf76b6225b622782dd12f89062b96
Harsh Sinha <harsh.sinha@pitt.edu>
2022-09-09 13:42:56 -0400

    Update docs with git tag

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.162.gdadd14d',
+    VERSION: '0.1.0+0.g1872ddd.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
