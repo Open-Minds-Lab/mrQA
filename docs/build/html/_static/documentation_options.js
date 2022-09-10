2f15dcaf374517bb0103075a9721a2cd11a79ed7
Harsh Sinha <harsh.sinha@pitt.edu>
2022-09-10 11:40:45 -0400

    Update docs

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.5+2.g7779592',
+    VERSION: '0.1.5+3.ga3946f7.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
