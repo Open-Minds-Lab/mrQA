b89b817b5895135cc25c2cdecbc152c59c491bb4
Harsh Sinha <harsh.sinha@pitt.edu>
2022-12-22 10:51:17 -0500

    add: function for testing equivalence of sequential vs parallel processing

--- mrQA/tests/test_parallel.py
+++ mrQA/tests/test_parallel.py
@@ -1,19 +1,51 @@
-
+from MRdataset import import_dataset, save_mr_dataset, load_mr_dataset
 from mrQA import check_compliance
-
-from mrQA import run_parallel
+from pathlib import Path
+from mrQA.run_parallel import process_parallel
+from MRdataset.config import MRDS_EXT
 
 dummy_DS = []
 
-def test_equivalence_seq_vs_parallel():
-    dataset = import_dataset(data_root=args.data_root,
-                             style=args.style,
-                             name=args.name,
-                             reindex=args.reindex,
-                             verbose=args.verbose,
-                             include_phantom=args.include_phantom,
-                             metadata_root=args.metadata_root)
-    check_compliance(dataset=dataset,
-                     strategy=args.strategy,
-                     output_dir=args.output_dir,
-                     reference_path=args.reference_path)
+
+def test_equivalence_seq_vs_parallel(data_source):
+    output_dir = Path(data_source) / 'test_mrqa_files'
+    output_path = {
+        'sequential': output_dir / ('sequential' + MRDS_EXT),
+        'parallel': output_dir / ('parallel' + MRDS_EXT)
+    }
+
+    sequential_ds = import_dataset(data_root=data_source,
+                                   style='dicom',
+                                   name='sequential')
+    save_mr_dataset(output_path['parallel'], sequential_ds)
+
+    process_parallel(data_source,
+                     output_dir,
+                     output_path['parallel'],
+                     'parallel')
+
+    # Generate a report for the merged dataset
+    parallel_ds = load_mr_dataset(output_path['parallel'], style='dicom')
+
+    report_path = {
+        'sequential': check_compliance(dataset=sequential_ds,
+                                       output_dir=output_dir),
+        'parallel': check_compliance(dataset=parallel_ds,
+                                     output_dir=output_dir)
+    }
+
+    compare(report_path['sequential'], report_path['parallel'])
+
+
+def compare(file1, file2):
+    with open(file1, 'r') as f:
+        d = set(f.readlines())
+
+    with open(file2, 'r') as f:
+        e = set(f.readlines())
+
+    open('file3.txt', 'w').close()  # Create the file
+
+    with open('file3.txt', 'a') as f:
+        for line in list(d - e):
+            f.write(line)
