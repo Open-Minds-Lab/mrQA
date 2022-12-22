698c4e1c43386a8abcc1183a6655b5ac3cfa8c2d
Harsh Sinha <harsh.sinha@pitt.edu>
2022-12-22 14:56:48 -0500

    minor : not yet working function

--- mrQA/tests/test_parallel.py
+++ mrQA/tests/test_parallel.py
@@ -4,6 +4,14 @@ from pathlib import Path
 from mrQA.run_parallel import process_parallel
 from MRdataset.config import MRDS_EXT
 from MRdataset.log import logger
+from mrQA.run_parallel import create_script, submit_job
+from MRdataset.utils import valid_paths
+from mrQA.parallel_utils import _make_file_folders
+from mrQA.utils import txt2list, list2txt
+import time
+import itertools
+from mrQA.run_merge import check_and_merge
+
 
 dummy_DS = []
 logger.setLevel('WARNING')
@@ -39,9 +47,17 @@ def test_equivalence_seq_vs_parallel(data_source):
     assert not diff(report_path['sequential'], report_path['parallel'])
 
 
-def diff(file1, file2):
-    with open(file1, 'r') as f:
-        lines_file1 = set(f.readlines())
+# def test_equivalence_in_combinations(data_source):
+#     subsets = itertools.combinations(mrds_files, 2)
+#     subset_dir = output_dir/'subsets'
+#     for i, item in enumerate(subsets):
+#         subset_filepath = subset_dir / f'subset{i}.txt'
+#         list2txt(subset_filepath, item)
+#         check_and_merge(
+#             mrds_list_filepath=subset_filepath,
+#             output_path=subset_dir / f'parallel{i}{MRDS_EXT}',
+#             name=f'subset{i}'
+#         )
 
     with open(file2, 'r') as f:
         lines_file2 = set(f.readlines())
