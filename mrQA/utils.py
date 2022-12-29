a64645025aa9b9fa5257b13b4f23d0cd0d3ace30
Harsh Sinha <harsh.sinha@pitt.edu>
2022-12-29 17:44:17 -0500

    Add:  add a function to sanitize filenames before saving to disk

--- mrQA/utils.py
+++ mrQA/utils.py
@@ -10,6 +10,8 @@ from itertools import groupby
 from itertools import takewhile
 from pathlib import Path
 from typing import Union, List
+import unicodedata
+import re
 
 import numpy as np
 from MRdataset.log import logger
@@ -251,6 +253,23 @@ def apply_round(dict_, decimals):
     for key, value in new_dict.items():
         new_dict[key] = round_if_number(value, decimals)
     return new_dict
+
+
+def slugify(value, allow_unicode=False):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 
 def is_integer_number(n: Union[int, float]) -> bool:
