d33dbaa37135ca967659c1c323c733c0864b8cfc
Harsh Sinha <harsh.sinha@pitt.edu>
2023-02-16 14:35:01 -0500

    reformat class

--- mrQA/tests/test_monitor.py
+++ mrQA/tests/test_monitor.py
@@ -15,17 +15,18 @@ class TestMonitorDummyDataset(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # Extract Zip folders
-        zip_path = Path('/home/sinhah/github/MRdataset/examples/test_merge_data.zip')
+        zip_path = Path(
+            '/home/sinhah/github/MRdataset/examples/test_merge_data.zip')
         temp_dir = Path('/tmp/')
-        extract_folder = temp_dir/zip_path.stem
-        cls.data_source = temp_dir/'test_merge_data'
+        extract_folder = temp_dir / zip_path.stem
+        cls.data_source = temp_dir / 'test_merge_data'
         if extract_folder.exists():
             shutil.rmtree(extract_folder)
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:
             zip_ref.extractall(temp_dir)
 
         # Set up output directories
-        output_dir = temp_dir/'output_dir'
+        output_dir = temp_dir / 'output_dir'
         if output_dir.is_dir():
             shutil.rmtree(output_dir)
         output_dir.mkdir(exist_ok=False, parents=True)
@@ -34,7 +35,7 @@ class TestMonitorDummyDataset(unittest.TestCase):
 
         # Read full dataset, acts as ground truth
         complete_dataset = import_dataset(
-            data_source=temp_dir/'test_merge_data/full_data',
+            data_source=temp_dir / 'test_merge_data/full_data',
             name='dummy_ds')
         report_path = check_compliance(complete_dataset,
                                        output_dir=output_folder_path)
@@ -42,19 +43,19 @@ class TestMonitorDummyDataset(unittest.TestCase):
         cls.complete_dataset = load_mr_dataset(fname)
 
     def test_modalities(self):
-        input_folder_path = self.data_source/'new_modalities'
+        input_folder_path = self.data_source / 'new_modalities'
         self.simulate(input_folder_path, self.output_folder_path)
 
     def test_subjects(self):
-        input_folder_path = self.data_source/'new_subjects'
+        input_folder_path = self.data_source / 'new_subjects'
         self.simulate(input_folder_path, self.output_folder_path)
 
     def test_sessions(self):
-        input_folder_path = self.data_source/'new_sessions'
+        input_folder_path = self.data_source / 'new_sessions'
         self.simulate(input_folder_path, self.output_folder_path)
 
     def test_runs(self):
-        input_folder_path = self.data_source/'new_runs'
+        input_folder_path = self.data_source / 'new_runs'
         self.simulate(input_folder_path, self.output_folder_path)
 
     @staticmethod
@@ -67,22 +68,16 @@ class TestMonitorDummyDataset(unittest.TestCase):
                 raise RuntimeError(e.decode('utf8'))
 
     def simulate(self, input_fpath, output_fpath):
-        ds1 = import_dataset(data_source=input_fpath/'set1',
+        ds1 = import_dataset(data_source=input_fpath / 'set1',
                              name='dummy_ds')
         check_compliance(ds1, output_dir=output_fpath)
 
         self.copy_new_scans(src=input_fpath / 'set2',
                             dest=input_fpath / 'set1')
 
-        last_record = get_last_valid_record(output_fpath)
-        # if last_record:
-        last_reported_on, last_fname = last_record
-
-        report_path = mrqa_monitor(name='dummy_ds',
-                                   data_source=input_fpath/'set1',
-                                   output_dir=output_fpath,
-                                   last_reported_on=last_reported_on,
-                                   last_fname=last_fname)
+        report_path = monitor(name='dummy_ds',
+                              data_source=input_fpath / 'set1',
+                              output_dir=output_fpath)
 
         mrds_path = mrds_fpath(output_fpath, report_path.stem)
         ds2 = load_mr_dataset(mrds_path)
