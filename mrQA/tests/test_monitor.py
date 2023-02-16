fe66a51a0d77de419cec8d6304d4492a219c3dcf
Harsh Sinha <harsh.sinha@pitt.edu>
2023-02-16 11:14:31 -0500

    move function is_same_dataset to MRdataset

--- mrQA/tests/test_monitor.py
+++ mrQA/tests/test_monitor.py
@@ -78,29 +78,5 @@ class TestMergeDatasets(unittest.TestCase):
         ds2 = load_mr_dataset(output_fpath/f"{fname}{MRDS_EXT}")
         assert self.is_same_dataset(ds2, self.complete_dataset)
 
-    @staticmethod
-    def is_same_dataset(dataset1, dataset2):
-        modalities_list1 = sorted(dataset1.modalities)
-        modalities_list2 = sorted(dataset2.modalities)
-        for modality1, modality2 in zip(modalities_list1, modalities_list2):
-            assert modality1.name == modality2.name
-            assert modality1.compliant == modality2.compliant
-            assert modality1._reference == modality2._reference
-            assert modality1.non_compliant_data.equals(modality2.non_compliant_data)
-            subjects_list1 = sorted(modality1.subjects)
-            subjects_list2 = sorted(modality2.subjects)
-            for subject1, subject2 in zip(subjects_list1, subjects_list2):
-                assert subject1.name == subject2.name
-                assert subject1.__dict__ == subject2.__dict__
-                sessions_list1 = sorted(subject1.sessions)
-                sessions_list2 = sorted(subject2.sessions)
-                for session1, session2 in zip(sessions_list1, sessions_list2):
-                    assert session1.name == session2.name
-                    assert session1.__dict__ == session2.__dict__
-                    runs_list1 = sorted(session1.runs)
-                    runs_list2 = sorted(session2.runs)
-                    for run1, run2 in zip(runs_list1, runs_list2):
-                        assert run1.__dict__ == run2.__dict__
-                        assert run1.name == run2.name
-                        assert run1.params == run2.params
-        return True
+
+
