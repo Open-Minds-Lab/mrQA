7145d57ffaf0fa0dbb0be2ded0416eadbfc73ed5
Harsh Sinha <harsh.sinha@pitt.edu>
2023-06-15 17:10:11 -0400

    monitor_project.py: > optimize_imports > change input txt file to input directory > remove empty folders > exclude folders if list is provided > use multiprocessing

--- examples/monitor_project.py
+++ examples/monitor_project.py
@@ -1,13 +1,13 @@
 import argparse
-import os.path
-import sys
+import multiprocessing as mp
 from pathlib import Path
+
+from MRdataset.config import DatasetEmptyException
+from MRdataset.log import logger
+from MRdataset.utils import valid_dirs
 
 from mrQA import monitor
 from mrQA.utils import txt2list
-from MRdataset.log import logger
-from MRdataset.utils import valid_dirs
-from MRdataset.config import DatasetEmptyException
 
 
 def main():
@@ -21,36 +21,60 @@ def main():
     optional = parser.add_argument_group('optional arguments')
 
     # Add help
-    required.add_argument('-d', '--datasets-txt', type=str, required=True,
-                          help='A txt file which contains a list of projects'
+    required.add_argument('-d', '--data-root', type=str, required=True,
+                          help='A folder which contains projects'
                                'to process')
     optional.add_argument('-o', '--output-dir', type=str,
                           default='/home/mrqa/mrqa_reports/',
                           help='specify the directory where the report'
-                               ' would be saved. By default, the --data_source '
-                               'directory will be used to save reports')
-
+                               ' would be saved')
+    optional.add_argument('-x', '--exclude-fpath', type=str,
+                          help='A txt file containing a'
+                               'list of folders to be skipped while'
+                               'monitoring')
     args = parser.parse_args()
-    if Path(args.datasets_txt).exists():
-        datasets_path = txt2list(args.datasets_txt)
+    if Path(args.data_root).exists():
+        data_root = Path(args.data_root)
+        non_empty_folders = []
+        for folder in data_root.iterdir():
+            if folder.is_dir() and any(folder.iterdir()):
+                non_empty_folders.append(folder)
     else:
-        raise ValueError("Need a valid path to a txt file, which consists of "
-                         f"names of projects to process. "
-                         f"Got {args.datasets_txt}")
-    dirs = valid_dirs(datasets_path)
+        raise ValueError("Need a valid path to a folder, which consists of "
+                         f"projects to process. "
+                         f"Got {args.data_root}")
 
-    for folder_path in dirs:
-        name = Path(folder_path).stem
-        print(f"\nProcessing {name}\n")
-        output_folder = Path(args.output_dir) / name
-        try:
-            monitor(name=name,
-                    data_source=folder_path,
-                    output_dir=output_folder,
-                    decimals=2,
-                    )
-        except DatasetEmptyException as e:
-            logger.warning(f'{e}: Folder {name} has no DICOM files.')
+    dirs = valid_dirs(non_empty_folders)
+
+    if len(non_empty_folders) < 2:
+        dirs = [dirs]
+    if args.exclude_fpath is not None:
+        if not Path(args.exclude_fpath).exists():
+            raise FileNotFoundError("Need a valid filepath to the exclude list")
+        exclude_filepath = Path(args.exclude_fpath).resolve()
+        skip_list = [Path(i).resolve() for i in txt2list(exclude_filepath)]
+
+        for fpath in dirs:
+            if Path(fpath).resolve() in skip_list:
+                dirs.remove(fpath)
+
+    pool = mp.Pool(processes=10)
+    arguments = [(f, args.output_dir) for f in dirs]
+    pool.starmap(run, arguments)
+
+
+def run(folder_path, output_dir):
+    name = Path(folder_path).stem
+    print(f"\nProcessing {name}\n")
+    output_folder = Path(output_dir) / name
+    try:
+        monitor(name=name,
+                data_source=folder_path,
+                output_dir=output_folder,
+                decimals=2,
+                )
+    except DatasetEmptyException as e:
+        logger.warning(f'{e}: Folder {name} has no DICOM files.')
 
 
 if __name__ == "__main__":
