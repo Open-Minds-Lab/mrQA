e568dadc1fd5e4134ba1fcc52e0433ba651fc62c
Harsh Sinha <harsh.sinha@pitt.edu>
2023-08-08 10:52:14 -0400

    Modify CompliantDataset, and NonCompliantDataset as per requirements for report generation

--- mrQA/base.py
+++ mrQA/base.py
@@ -14,29 +14,33 @@ class NonCompliantDataset(BaseDataset):
         super().__init__(name=name, data_source=None)
         self._nc_flat_map = {}
         self._nc_tree_map = {}
+        self._nc_params_map  = {}
 
-    def get_non_compliant_params(self, seq_id):
+    def get_non_compliant_param_ids(self, seq_id):
+        return list(self._nc_params_map[seq_id])
+
+    def get_non_compliant_param_values(self, seq_id, param_name=None):
         for subj in self._subj_ids:
             for sess in self._nc_tree_map[subj]:
                 if seq_id in self._nc_tree_map[subj][sess]:
                     for run in self._nc_tree_map[subj][sess][seq_id]:
-                        yield (subj, sess, run,
-                               self._nc_tree_map[subj][sess][seq_id][run])
+                            yield (subj, self._nc_tree_map[subj][sess][seq_id][run][param_name])
 
     def add_non_compliant_params(self, subject_id, session_id, run_id, seq_id,
             non_compliant_params):
         """adds a given subject, session or run to the dataset"""
+        for param in non_compliant_params:
+            if seq_id not in self._nc_params_map:
+                self._nc_params_map[seq_id] = set()
+            self._nc_params_map[seq_id].add(param.name)
 
-        if (subject_id, session_id, run_id, seq_id) not in self._nc_flat_map:
-            self._nc_flat_map[(subject_id, session_id, run_id, seq_id)] = []
-        self._nc_tree_add_node(subject_id, session_id, run_id, seq_id,
-                            non_compliant_params)
-        self._nc_flat_map[(subject_id, session_id, run_id, seq_id)].extend(
-            non_compliant_params
-        )
+            if (subject_id, session_id, run_id, seq_id, param.name) not in self._nc_flat_map:
+                self._nc_flat_map[(subject_id, session_id, run_id, seq_id, param.name)] = param
+            self._nc_tree_add_node(subject_id, session_id, run_id,
+                                   seq_id,param)
 
     def _nc_tree_add_node(self, subject_id, session_id, run_id, seq_name,
-                       non_compliant_params):
+                       param):
         """helper to add nodes deep in the tree
 
         hierarchy: Subject > Session > Sequence > Run
@@ -53,12 +57,10 @@ class NonCompliantDataset(BaseDataset):
             self._nc_tree_map[subject_id][session_id][seq_name] = dict()
 
         if run_id not in self._nc_tree_map[subject_id][session_id][seq_name]:
-            self._nc_tree_map[subject_id][session_id][seq_name][run_id] = []
+            self._nc_tree_map[subject_id][session_id][seq_name][run_id] = dict()
 
-        self._nc_tree_map[subject_id][session_id][seq_name][run_id].extend(
-            non_compliant_params
-        )
-
+        if param.name not in  self._nc_tree_map[subject_id][session_id][seq_name][run_id]:
+            self._nc_tree_map[subject_id][session_id][seq_name][run_id][param.name] = param
 
     def load(self):
         pass
