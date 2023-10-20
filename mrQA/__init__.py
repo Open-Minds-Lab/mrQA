716562377e4918fce9858a494cc7fb227657c9d8
Harsh Sinha <harsh.sinha@pitt.edu>
2023-10-20 17:40:19 -0400

    remove logger.py, add it to config.py

--- mrQA/__init__.py
+++ mrQA/__init__.py
@@ -2,21 +2,14 @@
 
 __author__ = """Harsh Sinha"""
 __email__ = 'harsh.sinha@pitt.edu'
-__version__ = '0.1.0'
+# __version__ = '0.1.0'
 
 import logging
 
-from mrQA.logger import INFO_FORMATTER, init_log_files
+from MRdataset.config import configure_logger
+
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARNING)
-# defines the stream handler
-_ch = logging.StreamHandler()  # creates the handler
-_ch.setLevel(logging.WARNING)  # sets the handler info
-# sets the handler formatting
-_ch.setFormatter(logging.Formatter(INFO_FORMATTER))
-# adds the handler to the global variable: log
-logger.addHandler(_ch)
-init_log_files(logger, mode='w')
+logger = configure_logger(logger, output_dir=None, mode='w')
 
 from mrQA.monitor import monitor # noqa
 from mrQA.project import check_compliance # noqa
--- mrQA/config.py
+++ mrQA/config.py
@@ -1,8 +1,51 @@
+import logging
+import tempfile
 from pathlib import Path
 
 from MRdataset import MRDS_EXT
 from MRdataset.config import MRException
 from protocol import UnspecifiedType
+
+
+def configure_logger(log, output_dir, mode='w', level='ERROR'):
+    """
+    Initiate log files.
+
+    Parameters
+    ----------
+    log : logging.Logger
+        The logger object.
+    mode : str, (``'w'``, ``'a'``)
+        The writing mode to the log files.
+        Defaults to ``'w'``, overwrites previous files.    """
+    console_handler = logging.StreamHandler()  # creates the handler
+    warn_formatter = ('%(filename)s:%(name)s:%(funcName)s:%(lineno)d:'
+                      ' %(message)s')
+    error_formatter = '%(asctime)s - %(levelname)s - %(message)s'
+    if output_dir is None:
+        output_dir = tempfile.gettempdir()
+    output_dir = Path(output_dir) / '.mrdataset'
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    warn_file = output_dir / 'warn.log'
+    if level == 'WARNING':
+        warn = logging.FileHandler(warn_file, mode=mode)
+        warn.setLevel(logging.WARN)
+        warn.setFormatter(logging.Formatter(warn_formatter))
+        log.addHandler(warn)
+
+    # keep only errors on console
+    console_handler.setLevel(logging.ERROR)  # sets the handler info
+    console_handler.setFormatter(logging.Formatter(error_formatter))
+    log.addHandler(console_handler)
+
+    error_file = output_dir / 'error.log'
+    error = logging.FileHandler(error_file, mode=mode)
+    error.setLevel(logging.ERROR)
+    error.setFormatter(logging.Formatter(error_formatter))
+    log.addHandler(error)
+    return log
+
 
 STRATEGIES_ALLOWED = ['majority', ]
 
@@ -15,8 +58,7 @@ PARAMETER_NAMES = [
     'EchoTrainLength',
     'PixelBandwidth',
     'NumberOfPhaseEncodingSteps',
-    ]
-
+]
 
 PATH_CONFIG = {
     'data_source': Path.home() / 'scan_data',
@@ -30,11 +72,11 @@ Unspecified = UnspecifiedType()
 
 
 def past_records_fpath(folder):
-    return Path(folder/'past_record.txt')
+    return Path(folder / 'past_record.txt')
 
 
 def status_fpath(folder):
-    return Path(folder/'non_compliance_log.txt')
+    return Path(folder / 'non_compliance_log.txt')
 
 
 def report_fpath(folder_path, fname):
@@ -81,6 +123,7 @@ class ComplianceException(Exception):
     Custom error that is raised when some critical properties are not
     found in dicom file
     """
+
     def __init__(self, message, **kwargs):
         super().__init__(message)
 
@@ -100,8 +143,6 @@ class ChangingParamsinSeries(ComplianceException):
     Custom error that is raised when parameter values are different for
     different slices even though the SeriesInstanceUID is same.
     """
-
-
     def __init__(self, filepath):
         super().__init__("Expected all dicom slices to have same parameters. "
                          "Got changing parameters : {}".format(filepath))
