be178b15b52343a915897df89176af2c61e49489
Harsh Sinha <harsh.sinha@pitt.edu>
2023-11-06 18:47:33 -0500

    add scatter plot and sitevsdate

--- mrQA/plotting.py
+++ mrQA/plotting.py
@@ -1,4 +1,6 @@
+import csv
 from collections import defaultdict
+from pathlib import Path
 
 from bokeh.embed import components
 from bokeh.plotting import figure
@@ -53,7 +55,7 @@ class MultiPlot(BasePlot):
         if not normalized_counts:
             raise ValueError("Primary counter is empty. "
                              "No values found for normalization")
-        self.uniq_secondary_values = uniq_secondary_values
+        self.uniq_secondary_values = sorted(list(uniq_secondary_values))
         return dict(sorted(normalized_counts.items()))
 
     def pad_with_zeroes(self, normalized_counts, primary_param):
@@ -143,7 +145,62 @@ class MultiLinePlot(MultiPlot):
         self.div, self.script = self.get_plot_components(data)
 
 
-class ManufacturerAndDate(MultiLinePlot):
+class MultiScatterPlot(MultiPlot):
+    _name = 'multi_scatter'
+
+    def __init__(self, legend_label=None, y_axis_label=None,  size=10,
+                 alpha=0.75, plot_height=300,
+                 plot_width=800):
+        super().__init__(name=self._name)
+        self.legend_label = legend_label
+        self.size = size
+        self.alpha = alpha
+        self.y_axis_label = y_axis_label
+        self.plot_width = plot_width
+        self.plot_height = plot_height
+
+    def get_plot_components(self, data):
+        label = list(data.keys())[0]
+        self.set_cmap(len(self.uniq_secondary_values))
+
+        p = figure(x_range=self.x_range,
+                   y_axis_label=self.y_axis_label,
+                   width=self.plot_width, height=self.plot_height)
+        for i, k in enumerate(self.uniq_secondary_values):
+            try:
+                p.circle(x=label, y=k, source=data, size=self.size,
+                         alpha=self.alpha,
+                         color=self.colors[i], legend_label=k)
+            except IndexError:
+                print(f"Unable to plot {k}, Color index {i} out of range")
+
+        p.xaxis.major_label_orientation = "vertical"
+        p.xgrid.grid_line_color = None
+        p.ygrid.grid_line_alpha = 0.5
+        p.legend.click_policy = "hide"
+        p.add_layout(p.legend[0], 'below')
+        return components(p)
+
+    def plot(self, non_compliant_ds, complete_ds, parameters=None):
+        """Creates a plot for the given data"""
+        if not parameters:
+            parameters = self.parameters
+        data = self.compute_counts(non_compliant_ds, complete_ds,
+                                   parameters)
+        self.width = 0.9
+        self.x_range = data[parameters[0]]
+        x = data[parameters[0]]
+        for i in parameters:
+            if 'date' in i.lower():
+                self.x_range = (previous_month(min(x)), next_month(max(x)))
+            if 'age' in i.lower():
+                self.x_range = [min(x) - 0.1, max(x) + 0.1]
+                # self.width = timedelta(days=1)
+
+        self.div, self.script = self.get_plot_components(data)
+
+
+class ManufacturerAndDate(MultiScatterPlot):
     """Plot for Manufacturer and Date"""
     def __init__(self):
         super().__init__(plot_height=600, plot_width=800)
@@ -157,7 +214,7 @@ class PatientSexAndAge(MultiLinePlot):
         self.parameters = ['PatientAge', 'PatientSex']
 
 
-class ManufacturersModelAndDate(MultiLinePlot):
+class ManufacturersModelAndDate(MultiScatterPlot):
     """Plot for Manufacturer and Date"""
     def __init__(self):
         super().__init__(plot_height=600, plot_width=800)
@@ -191,7 +248,7 @@ class ManufacturersModelAndDate(MultiLinePlot):
         return counter
 
 
-class SoftwareVersionsAndDate(MultiLinePlot):
+class SoftwareVersionsAndDate(MultiScatterPlot):
     """Plot for Manufacturer and Date"""
     def __init__(self):
         super().__init__(plot_height=600, plot_width=800)
@@ -211,6 +268,62 @@ class SoftwareVersionsAndDate(MultiLinePlot):
                     secondary_value = seq[param_secondary].get_value()
                     manufacturer = seq['Manufacturer'].get_value()
                     secondary_value = f'{manufacturer} {secondary_value}'
+                except KeyError:
+                    continue
+
+                if (isinstance(primary_value, UnspecifiedType) or
+                    isinstance(secondary_value, UnspecifiedType)):
+                    continue
+                if primary_value not in counter:
+                    counter[primary_value] = {}
+                if secondary_value not in counter[primary_value]:
+                    counter[primary_value][secondary_value] = 0
+                counter[primary_value][secondary_value] += 1
+        return counter
+
+
+class SiteAndDate(MultiScatterPlot):
+    """Plot for Manufacturer and Date"""
+    def __init__(self):
+        super().__init__(plot_height=600, plot_width=800)
+        self.parameters = ['ContentDate', 'InstitutionName']
+        self.csv_path = Path('/media/sinhah/extremessd/ABCD/1210908/original_files/abcd_lt01.txt')
+        self.subject_site_map = self.get_subject_site_map()
+
+    def get_subject_site_map(self):
+        """Returns a dictionary mapping subject to site"""
+        subject_site_map = {}
+        with open(self.csv_path, 'r') as fh:
+            reader = csv.DictReader(fh, delimiter='\t')
+            for i, row in enumerate(reader):
+                if i == 0:
+                    continue
+                subject_id = row['subjectkey']
+                follow_up = row['eventname']
+                site = row['site_id_l']
+                if 'baseline' in follow_up.lower():
+                    subject_site_map[subject_id] = site
+        return subject_site_map
+
+    def get_subject_site(self, subject_id):
+        """Returns the site for the given subject"""
+        return self.subject_site_map[subject_id]
+
+    def get_counter(self, dataset, parameters):
+        """Computes the counter for the given dataset and parameters."""
+        counter = {}
+        if len(parameters) > 2:
+            raise ValueError("MultiPlot can only plot two parameters")
+        param_primary, param_secondary = parameters
+        for seq_name in dataset.get_sequence_ids():
+            for subj, sess, run, seq in dataset.traverse_horizontal(
+                    seq_name):
+                try:
+                    primary_value = seq[param_primary].get_value()
+                    subject_value = seq.subject_id
+                    secondary_value = self.get_subject_site(subject_value)
+                    # manufacturer = seq['Manufacturer'].get_value()
+                    # secondary_value = f'{manufacturer} {secondary_value}'
                 except KeyError:
                     continue
 
